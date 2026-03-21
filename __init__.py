5ae3074b550902b01db966ba121b95a3cf36d859
tesseract <usestesting141@gmail.com>
2026-03-21 21:35:36 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,13 +1,5 @@
-"""
-ncr-cipher: combinatorics-based symmetric encryption.
-"""
 from ncr_cipher.core import (
-    NCRKey,
-    NCRError,
-    NCRAuthError,
-    NCRFormatError,
-    VERSION,
+    NCRKey, NCRError, NCRAuthError, NCRFormatError, VERSION,
 )
-
 __version__ = VERSION
 __all__ = ["NCRKey", "NCRError", "NCRAuthError", "NCRFormatError", "__version__"]
